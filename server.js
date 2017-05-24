e9a63ec97a5933ad29be06ba1258bb45dcf4d440
Eden <edenstevens101@gmail.com>
2017-05-24 10:44:38 +0100

    Populated Server File

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(require('./controllers/index.js'));
+  app.use(express.static('client/build'));
+
+  app.listen(3000, function(){
+    console.log('Running smoothly on port: '+this.address().port)
+  });
+
+
+
